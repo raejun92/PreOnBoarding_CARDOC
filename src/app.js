61d8df1a0a159700a572f4594c0fbc125782a532
raejun92 <cvcv1324@naver.com>
2021-11-23 14:28:34 +0900

    [Chore]: 디렉토리 구조 생성 - 기본 구조를 설계

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from ('express');
+import path from ('path');
+import cookieParser from ('cookie-parser');
+import morgan from ('morgan');
+
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
